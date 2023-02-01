f10587655ddf43676161d1e18cb3c9a48eff74df
heesun <heesun9793@naver.com>
2023-02-01 17:36:05 +0900

    데이터 중복 체크

--- src/main/java/shop/mtcoding/ajax/controller/userController.java
+++ src/main/java/shop/mtcoding/ajax/controller/userController.java
@@ -21,7 +21,7 @@ public class userController {
             return new ResponseDto<>(-1, "username 전달 바람", null);
         }
 
-        if (username.equals("cos")) {
+        if (username.equals("ssar")) {
             return new ResponseDto<>(1, "동일한 username 존재", false);
         } else {
             return new ResponseDto<>(1, "username 사용 가능", true);
